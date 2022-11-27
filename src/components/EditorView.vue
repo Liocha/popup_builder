ff67a7ab344f80753be9d3bc14e82d26f22792e4
Aleksey <dev@liocha.ru>
2022-11-27 05:17:47 +0300

    firstStep

---
+++ src/components/EditorView.vue
@@ -0,0 +1,47 @@
+<script lang="jsx">
+import CellWrapperVue from "@/components/CellWrapper.vue";
+import PluginDropFileVue from "@/components/PluginDrop.vue";
+
+export default {
+  props: {
+    value: Array,
+    parentId: String,
+    parent: Object,
+  },
+  methods: {
+    /**
+     * @type {HTMLElement}
+     */
+    getWrapperElement() {
+      return this.$refs.editor
+    },
+  },
+
+  render() {
+    return (
+      <div class={`vs-editor ${this.parentId ? 'nest-editor' : 'root-editor'}`}>
+        <div class="vs-editor-canvas" ref="editor">
+          {this.value.map((item) => {
+            return <CellWrapperVue key={item.id} item={item} />
+          })}
+          {/***Inject Plugins**/}
+          {this.$slots.default}
+          <PluginDropFileVue parentId={this.parentId} length={this.value.length} />
+        </div>
+      </div>
+    )
+  },
+}
+</script>
+
+<style lang="less">
+.vs-editor-canvas {
+  width: 100%;
+  height: 100%;
+}
+
+.root-editor > .vs-editor-canvas {
+  transform: scale(1);
+  transform-origin: top left;
+}
+</style>
---
+++ src/components/PropNumberInput.vue
@@ -0,0 +1,18 @@
+<script lang="jsx">
+export default {
+  props: {
+    value: Number,
+  },
+
+  methods: {
+    handleChange(v) {
+      this.$emit('input', +v)
+    },
+  },
+  render() {
+    return (
+      <input class="input-value" value={this.value} onInput={(e) => this.handleChange(e.target.value)} type="number" />
+    )
+  },
+}
+</script>
